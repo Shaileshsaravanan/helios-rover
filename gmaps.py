639963e62eda8ebbe0fea387267030547d27dc62
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-07-06 16:49:35 +0530

    gmaps update

--- gmaps.py
+++ gmaps.py
@@ -33,3 +33,47 @@ api_key = 'YOUR_GOOGLE_MAPS_API_KEY'
 start_location = 'START_LOCATION_ADDRESS_OR_COORDINATES'
 end_location = 'END_LOCATION_ADDRESS_OR_COORDINATES'
 path_coordinates = get_path_coordinates(api_key, start_location, end_location)
+
+# kml stuff
+
+from fastkml import kml
+from io import StringIO
+
+kml_data = '''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+  <Document>
+    <Placemark>
+      <LineString>
+        <coordinates>
+          150.644,-34.397,0
+          150.644,-34.397,0
+          <!-- Add more coordinates as needed -->
+        </coordinates>
+      </LineString>
+    </Placemark>
+  </Document>
+</kml>'''
+
+def move_rover_to_coordinate(lat, lng):
+    print(f"Moving rover to latitude: {lat}, longitude: {lng}")
+
+def process_kml(kml_string):
+    k = kml.KML()
+    k.from_string(kml_string)
+    
+    for feature in k.features():
+        for placemark in feature.features():
+            if hasattr(placemark.geometry, 'geoms'):
+                for line in placemark.geometry.geoms:
+                    coords = list(line.coords)
+                    for coord in coords:
+                        lng, lat, _ = coord
+                        move_rover_to_coordinate(lat, lng)
+            else:
+                coords = list(placemark.geometry.coords)
+                for coord in coords:
+                    lng, lat, _ = coord
+                    move_rover_to_coordinate(lat, lng)
+
+# Simulating the rover processing the KML data
+process_kml(kml_data)
